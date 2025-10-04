4ff1d398bb4a6313478f52180e098c329e05e187
hraj94398-wq <hraj94398@gmail.com>
2025-10-04 14:38:11 +0530

    Add files via upload

---
+++ bdy_wish.py
@@ -0,0 +1,18 @@
+import time
+
+name = "swati"
+def birthday_wish(name):
+    print("🎉🎂 Happy Birthday swati 🎂🎉")
+    time.sleep(1)
+    print(f"\nDear {name},")
+    time.sleep(1)
+    print("Wishing you a day filled with love, laughter, and joy!")
+    time.sleep(1)
+    print("May your year ahead be as wonderful as you are.")
+    time.sleep(1)
+    print("You're not just my sister, you're my best friend 💖")
+    time.sleep(1)
+    print("\n🎈🎁🎉 Happy Birthday! 🎉🎁🎈")
+
+
+birthday_wish("Swati")
